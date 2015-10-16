32b3d819a46f767bdb9fc8a5b42d998eabb3037e
Pulkit Chaudhary <chaudharypulkit93@gmail.com>
2015-10-16 22:01:54 +0530

    Writing unit tests

--- treeTests.py
+++ treeTests.py
@@ -4,34 +4,41 @@ from tree import Tree
 class TestTree(unittest.TestCase):
   def setUp(self):
     self.tree = Tree()
+    self.tree.insertData(3)
+    self.tree.insertData(1)
+    self.tree.insertData(7)
+    self.tree.insertData(5)
 
   def tearDown(self):
     self.tree = None
 
   def testInsertData(self):
     root = self.tree.root
-    self.tree.insertData(3)
     self.assertEqual(root.data, 3)
-    self.tree.insertData(1)
     self.assertEqual(root.lchild.data, 1)
-    self.tree.insertData(7)
     self.assertEqual(root.rchild.data, 7)
-    self.tree.insertData(5)
     self.assertEqual(root.rchild.lchild.data, 5)
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+  def testDelete(self):
+    root = self.tree.root
+    self.tree.delete(root, 3)
+    self.assertEqual(root.data, 1)
+    self.tree.delete(root, 7)
+    self.assertEqual(root.rchild.data, 5)
+    self.tree.delete(root, 5)
+    self.assertEqual(root.rchild, None)
 
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
+  def testIsKeyPresent(self):
+    root = self.tree.root
+    isKeyPresent = self.tree.isKeyPresent(root, 3)
+    self.assertTrue(isKeyPresent)
+    isKeyPresent = self.tree.isKeyPresent(root, 9)
+    self.assertFalse(isKeyPresent)
 
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
+  def testLargest(self):
+    root = self.tree.root
+    largest = self.tree.largest(root)
+    self.assertEqual(largest.data, 7)
 
 if __name__ == '__main__':
   suite = unittest.TestLoader().loadTestsFromTestCase(TestTree)
